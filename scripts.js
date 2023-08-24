ee7c66dd91c2bc14048f4c2e3ab960dc855c83a3
Gintautas Poderys <gintautasp25@gmail.com>
2023-08-24 23:00:41 +0300

    added GUI

--- scripts.js
+++ scripts.js
@@ -10,8 +10,8 @@ function getComputerChoice(){
         return 'computer brain broke'
     }
 }
-
 function getPlayerSelection() {
+
     choice = prompt('Rock Paper Scissors?')
     t = choice.toLowerCase()
     if (t == 'rock' || t == 'paper' || t == 'scissors'){
@@ -22,29 +22,39 @@ function getPlayerSelection() {
     }
 }
 
+let playerScore = 0
+let computerScore = 0
+
 function playRound(pl,pc) {
+    
     console.log('Player chose : "' + pl)
     console.log('Computer chose : "' + pc)
     if (pc === pl) {
-        return 'Round ended in a tie!'
+        return 'Round ended in a tie! The computer has picked ' + pc
     } else if (pc === 'rock' && pl === 'paper' ) {
-        return 'Player has won!'
+        playerScore += 1
+        return 'Player has won! The computer has picked ' + pc
     } else if (pc === 'rock' && pl === 'scissors' ) {
-        return 'Computer has won!'
+        computerScore += 1
+        return 'Computer has won!  The computer has picked ' + pc
     } else if (pc === 'paper' && pl === 'scissors' ) {
-        return 'Player has won!'
+        playerScore += 1
+        return 'Player has won!  The computer has picked ' + pc
     } else if (pc === 'paper' && pl === 'rock' ) {
-        return 'Computer has won!'
+        computerScore += 1
+        return 'Computer has won! The computer has picked ' + pc
     } else if (pc === 'scissors' && pl === 'paper' ) {
-        return 'Computer has won!'
+        computerScore += 1
+        return 'Computer has won! The computer has picked ' + pc
     } else if (pc === 'scissors' && pl === 'rock' ) {
-        return 'Player has won!'
+        playerScore += 1
+        return 'Player has won! The computer has picked ' + pc
     } else {
         return 'Round did not happen'
     }
 } 
-
 function playGame(){
+
     console.log('Game starts!')
     let playerScore = 0, computerScore = 0
     for (let i = 0; i < 5; i++) {
@@ -66,5 +76,63 @@ function playGame(){
         console.log('Game has ended in a tie!')
     }
 }
+const result = document.querySelector('.result')
+const score = document.querySelector('.score')
 
-playGame()
+const rockButton = document.querySelector('.rock')
+rockButton.addEventListener('click', () => {
+    result.textContent = playRound('rock',getComputerChoice())
+})
+const paperButton = document.querySelector('.paper')
+paperButton.addEventListener('click', () => {
+    result.textContent = playRound('paper',getComputerChoice())
+})
+const scissorsButton = document.querySelector('.scissors')
+scissorsButton.addEventListener('click', () => {
+    result.textContent = playRound('scissors',getComputerChoice())
+})
+const buttonPress = document.querySelectorAll('button')
+
+let toggle = 0
+let winner = ''
+buttonPress.forEach(button => {
+    button.addEventListener('click', () => {
+        score.textContent = `Score:  You ${playerScore} Computer ${computerScore}`
+        if (toggle ===0 && (playerScore === 5 || computerScore === 5)){
+            const bodySelect = document.querySelector('body')
+            const newDiv = document.createElement('div')
+            if (playerScore > computerScore){
+                winner = 'Player'
+            } else {winner = 'Computer'}
+            newDiv.textContent = `Game Over. ${winner}, has won.`
+            bodySelect.appendChild(newDiv)
+            toggle =1
+            score.classList.remove('score')
+        } 
+    })
+})
+
+
+
+
+
+
+
+
+// for (let i = 0; i < 1; i++) {
+//     console.log('Current score : Player', playerScore, computerScore,' Computer')
+//     winner = 0
+//     if (winner === 'Player has won!'){
+//         playerScore += 1
+//     } else if (winner === 'Computer has won!'){
+//         computerScore += 1
+//     }
+// }
+// console.log('Final score :', playerScore, computerScore)
+// if (playerScore > computerScore) {
+//     console.log('Player has won the game!')
+// } else if (computerScore > playerScore) {
+//     console.log('Computer has won the game!')
+// } else {
+//     console.log('Game has ended in a tie!')
+// }
